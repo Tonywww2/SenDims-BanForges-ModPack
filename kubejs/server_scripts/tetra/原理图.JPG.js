98ead11535b802f390f46a7f3909636e96ece61c
yiran1457 <3270897489@qq.com>
2026-04-09 20:29:18 +0800

    添加原理图构建代码实例

---
+++ kubejs/server_scripts/tetra/原理图.JPG.js
@@ -0,0 +1,57 @@
+let someArgs={
+    path :"slashblade/blade",
+    texturePath:"slashbladetetra:textures/gui/texture.png"
+}
+ServerEvents.highPriorityData(event=>{
+    event.addJson(`tetra:improvements/${someArgs.path}/test.json`,[
+        {
+            "key": "zzz",
+            "level": 1,
+            "effects": {
+                "RefineStrengthening":1
+            }
+        }
+    ])
+    event.addJson(`tetra:schematics/${someArgs.path}/test.json`,{
+        "replace": true,
+        "slots": [
+            someArgs.path
+        ],
+        "hone": true,
+        "rarity": "hone",
+        "displayType": "major",
+        "glyph": {
+            "textureX": 16,
+            "textureY": 0,
+            "textureLocation":  someArgs.texturePath
+        },
+        "requirement": {
+            "type": "tetra:and",
+            "requirements": [
+                {
+                    "type": "tetra:locked",
+                    //原理图的id
+                    "key": "tetra:warforge/axe"
+                }/*,//限制与其他improvement不兼容
+                {
+                    "type": "tetra:not",
+                    "requirement": {
+                        "type": "tetra:improvement",
+                        "improvement": "double/warforged"
+                    }
+                }*/
+            ]
+        },
+        "outcomes": [
+            {
+                "requiredTools": {
+                    "hammer_dig": 1
+                },
+                "experienceCost": 3,
+                "improvements": {
+                    "zzz": 1
+                }
+            }
+        ]
+    })
+})
