3434d04844f3737ca8f6ce0299b38dd146265a26
Tonywww <1204484415@qq.com>
2026-04-30 01:12:09 -0700

    再见了mcr

--- kubejs/server_scripts/miscs/overworld_control.js
+++ kubejs/server_scripts/miscs/overworld_control.js
@@ -9,41 +9,73 @@ let X_MAX = 15;
 let Z_MIN = 0;
 let Z_MAX = 15;
 
+let WALL_THICKNESS = 1;
+let ROOM_HEIGHT = 3;
+
+let Y_MIN = PLATFORM_Y - 1;
+let Y_MAX = PLAYER_Y + ROOM_HEIGHT + 1;
+
 let SPAWN_X = 8;
 let SPAWN_Z = 8;
 
 let MIN_Y_LEVEL = 280;
 
+let signs = [
+    // 北墙 (朝向南)
+    { pos: new BlockPos(7, PLAYER_Y + 1, 2), facing: 'south' },
+    { pos: new BlockPos(8, PLAYER_Y + 1, 2), facing: 'south' },
+    // 南墙 (朝向北)
+    { pos: new BlockPos(7, PLAYER_Y + 1, 13), facing: 'north' },
+    { pos: new BlockPos(8, PLAYER_Y + 1, 13), facing: 'north' },
+    // 西墙 (朝向东)
+    { pos: new BlockPos(2, PLAYER_Y + 1, 7), facing: 'east' },
+    { pos: new BlockPos(2, PLAYER_Y + 1, 8), facing: 'east' },
+    // 东墙 (朝向西)
+    { pos: new BlockPos(13, PLAYER_Y + 1, 7), facing: 'west' },
+    { pos: new BlockPos(13, PLAYER_Y + 1, 8), facing: 'west' }
+];
+
 let PLAYER_INIT_KEY = 'sdbf.linit';
 let OVERWORLD_STAGE = 'sdbf.world_lock';
 
 const buildBedrockPlatform = (level) => {
+
     for (let x = X_MIN; x <= X_MAX; x++) {
-        for (let z = Z_MIN; z <= Z_MAX; z++) {
-            level.setBlock(new BlockPos(x, PLATFORM_Y, z), Blocks.BEDROCK.defaultBlockState(), 3);
-            level.setBlock(new BlockPos(x, PLATFORM_Y - 1, z), Blocks.BEDROCK.defaultBlockState(), 3);
-            level.setBlock(new BlockPos(x, PLAYER_Y, z), Blocks.AIR.defaultBlockState(), 3);
-            level.setBlock(new BlockPos(x, PLAYER_Y + 1, z), Blocks.AIR.defaultBlockState(), 3);
+        for (let y = Y_MIN; y <= Y_MAX; y++) {
+            for (let z = Z_MIN; z <= Z_MAX; z++) {
+                let isWallX = (x < X_MIN + WALL_THICKNESS) || (x > X_MAX - WALL_THICKNESS);
+                let isWallZ = (z < Z_MIN + WALL_THICKNESS) || (z > Z_MAX - WALL_THICKNESS);
+                let isFloor = (y <= PLATFORM_Y);
+
+                if (isWallX || isWallZ || isFloor) {
+                    level.setBlock(new BlockPos(x, y, z), Blocks.BARRIER.defaultBlockState(), 3);
+                } else {
+                    level.setBlock(new BlockPos(x, y, z), Blocks.AIR.defaultBlockState(), 3);
+                }
+            }
         }
     }
 }
 
 const placeSign = (level) => {
-    let signPos = new BlockPos(SPAWN_X, PLAYER_Y, SPAWN_Z - 1);
 
-    level.setBlock(signPos, Blocks.OAK_SIGN.defaultBlockState(), 3);
+    signs.forEach(s => {
+        let block = level.getBlock(s.pos);
+        block.set('minecraft:oak_wall_sign', { facing: s.facing });
 
-    let be = level.getBlockEntity(signPos);
-    be.updateText(text =>
-        text
-            .setMessage(0, Text.of('To Begin'))
-            .setMessage(1, Text.of('Enter /skyblock gui'))
-            .setMessage(2, Text.of('开始'))
-            .setMessage(3, Text.of('输入指令 /skyblock gui'))
-        , true
-    );
-    be.setChanged();
-    return;
+        let be = level.getBlockEntity(s.pos);
+        if (be) {
+            be.updateText(text =>
+                text
+                    .setMessage(0, Text.of('To Begin'))
+                    .setMessage(1, Text.of('Enter /skyblock gui'))
+                    .setMessage(2, Text.of('开始'))
+                    .setMessage(3, Text.of('输入指令 /skyblock gui'))
+                , true
+            );
+            be.setChanged();
+        }
+    });
 
 }
 
@@ -75,9 +107,9 @@ PlayerEvents.loggedIn(event => {
 const punishPlayer = (player) => {
     player.teleportTo(TARGET_DIM, SPAWN_X + 0.5, PLAYER_Y, SPAWN_Z + 0.5, 0, 0);
 
-    player.potionEffects.add('minecraft:wither', 200, 0);
-    player.potionEffects.add('minecraft:blindness', 200, 0);
-    player.potionEffects.add('minecraft:nausea', 200, 0);
+    player.potionEffects.add('minecraft:wither', 100, 0);
+    player.potionEffects.add('minecraft:blindness', 100, 0);
+    player.potionEffects.add('minecraft:nausea', 100, 0);
 };
 
 PlayerEvents.tick(event => {
