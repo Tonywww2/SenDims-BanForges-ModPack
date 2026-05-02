1614c312f8a813329fde0d43da52c8a38e5a31bb
Tonywww <1204484415@qq.com>
2026-05-01 19:30:41 -0700

    b测前

--- kubejs/server_scripts/materials_merge.js
+++ kubejs/server_scripts/materials_merge.js
@@ -95,7 +95,8 @@ global.materialReplaceRules.set('minecraft:iron_ingot', {
         'ad_astra:space_pants',
         'ad_astra:space_boots',
         'artifacts:vampiric_glove',
-        'confluence:brain_of_confusion'
+        'confluence:brain_of_confusion',
+        "botania:manasteel_ingot"
     ]
 });
 
